0edd8088ad67c93a74a8632c99733b94121d4c95
Jacob <premojacob@gmail.com>
2021-01-25 18:19:59 -0500

    Fixed seeder file

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,7 +5,7 @@ from app.models import db, User
 def seed_users():
 
     demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+                password='password', firstName='Demo', lastName='User')
 
     db.session.add(demo)
 
