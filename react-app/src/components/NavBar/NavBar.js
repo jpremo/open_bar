e43a908319b7d0ef5921555938ddb8234069125a
Nick <nikhilmenon@comcast.net>
2021-01-26 12:32:36 -0600

    Top Nav Bar Rough Look

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import './NavBar.css';
+import picture from "./open_bar_logo.png"
 
 const NavBar = ({ setAuthenticated }) => {
   return (
     <div id="top-nav-bar">
-      <ul>
+       <NavLink exact to="/">
+        <img id="nav-bar-logo-picture" src={picture}/>
+         </NavLink>
+      <div id="nav-bar-menu">
           <NavLink to="/" exact={true} className="nav-link" activeClassName="active">
             Home
           </NavLink>
-       
           <NavLink to="/login" exact={true} className="nav-link" activeClassName="active">
             Login
           </NavLink>
-    
           <NavLink to="/sign-up" exact={true} className="nav-link" activeClassName="active">
             Sign Up
           </NavLink>
-     
           <NavLink to="/users" exact={true}  className="nav-link" activeClassName="active">
             Users
           </NavLink>
-       
           <LogoutButton setAuthenticated={setAuthenticated} />
-      </ul>
+      </div>
     </div>
   );
 }
