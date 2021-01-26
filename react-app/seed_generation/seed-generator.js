16430e80afed382031794ff43b9a249b27d9f532
Erick-Bravo <erickbrvo@gmail.com>
2021-01-26 10:38:52 -0800

    seeder generation for users, bars and reviews

--- react-app/seed_generation/seed-generator.js
+++ react-app/seed_generation/seed-generator.js
@@ -15,10 +15,25 @@ const randomArr = (arr) => {
   return arr[getRandom(0, arr.length - 1)]
 }
 
+
+const getMealImage = async () => {
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+  const data = await res.json()
+  if(data) return data.meals[0].strMealThumb
+  return "https://www.themealdb.com/images/media/meals/rjhf741585564676.jpg"
+}
+
+const getDrinkImage = async () => {
+  const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+  const data = await res.json()
+  if(data) return data.drinks[0].strDrinkThumb
+  return "https://www.thecocktaildb.com/images/media/drink/iuwi6h1504735724.jpg"
+}
+
 const makeUsers = async () => {
   const users = []
   for(let i = 0; i<100; i++) {
-      const profileImage = faker.image.imageUrl()
+      const profileImage = await getDrinkImage()
       users.push({
           email: faker.internet.email(),
           username: faker.internet.userName(),
@@ -31,55 +46,67 @@ const makeUsers = async () => {
   return users
 }
 
-const getMealImage = async () => {
-  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-  const data = await res.json()
-  if(data) return data.meals[0].strMealThumb
-  return "https://www.themealdb.com/images/media/meals/rjhf741585564676.jpg"
-}
+const getImage = async () => {
+  const rand = getRandom(1, 10)
 
-const getDrinkImage = async () => {
-  const res = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-  const data = await res.json()
-  if(data) return data.meals[0].strDrinkThumb
-  return "https://www.thecocktaildb.com/images/media/drink/iuwi6h1504735724.jpg"
+  if(rand > 7) {
+    return await getMealImage()
+  } else {
+    return await getDrinkImage()
+  }
 }
 
 let postNumber = 100;
 
-const generateCatPost = async (id) => {
-  const breed = await getCatBreed()
+const generateBar = async (id) => {
+  
   const images = []
-  const imNum = getRandom(1, 5)
-  const catAdjectives = ['Kitty Cat', 'Chonkzilla', 'Floofykin', 'Meowy Meowy Cat', 'Cat', 'Kitten']
-  const catPrefixes = ['Purry', 'Happy', 'Lazy', 'Goofy', 'Funny', 'Silly', 'Adorable', 'Cute']
+  const imNum = getRandom(5, 20)
+  
+  
   for (let i = 0; i < imNum; i++) {
-    const url = await getCatImage(breed.id)
-    images.push({url, title:`${randomArr(catPrefixes)} ${randomArr(catAdjectives)}`, locationId:id, userId:2})
+    const photoUrl = await getImage()
+    images.push({photoUrl, barId:id})
   }
 
-  const catRelated = ['Kitty', 'Cat', 'Chonker', 'Meowser', 'Catnip', 'Purr Purr']
+  const barRelated = ['Hops', 'Bottom Barrel', 'Chonker', 'Craft', 'Fantastic Fermenters', 'Grainy', 'Lauter Tun', 'Family Owned']
   const addressSuffix = ['Ln.', 'St.', 'Ct.', 'Blvd.', 'Rd.']
-  const businessArr = ['Pizzeria', 'Ice Cream Parlor', 'Repairs', 'Auto Dealer', 'Restaurant', 'Lawn Care']
+  const businessArr = ['Bar', 'Tavern', 'Winery', 'Dive Bar', 'Pirate Bar', 'Dance Bar', 'Ale House', 'Brewery', 'Canteen', 'Pub']
   let extraInfo = {
     businessCategory: randomArr(businessArr),
-    catbreed: breed
   }
+  const bannerImg = await getDrinkImage()
   const businessInfo = {
-    userId: 2,
+    
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-    address: `${getRandom(100, 10000)} ${randomArr(catRelated)} ${randomArr(addressSuffix)}`,
-    name: `${randomArr(catRelated)} ${extraInfo.businessCategory}`,
-    businessCategory: null,
-    petCategory: null,
-    coordinates: JSON.stringify({ lat: getRandom(392629, 393201) / 10000, lng: getRandom(-767587, -764449) / 10000, zoom: getRandom(12, 18) })
+    name: `${randomArr(barRelated)} ${extraInfo.businessCategory}`,
+    phoneNumber: faker.phone.phoneNumber(),
+    longitude: getRandom(-739638, -739066) / 10000,
+    latitude: getRandom(407020, 407592) / 10000,
+    street: `${getRandom(100, 10000)} ${randomArr(barRelated)} ${randomArr(addressSuffix)}`,
+    state: 'NY',
+    zipcode: getRandom(10002, 10007),
+    barSeats: getRandom(4, 20),
+    dayAndTime: JSON.stringify({
+      monday: `${getRandom(12, 16)}-${getRandom(0, 2)}`,
+      tuesday: `${getRandom(12, 16)}-${getRandom(0, 2)}`,
+      wednesday: `${getRandom(12, 16)}-${getRandom(0, 2)}`,
+      thursday: `${getRandom(12, 16)}-${getRandom(0, 2)}`,
+      friday: `${getRandom(10, 16)}-${getRandom(0, 4)}`,
+      saturday: `${getRandom(10, 16)}-${getRandom(0, 4)}`,
+      sunday: `${getRandom(12, 16)}-${getRandom(0, 2)}`,
+
+    }),
+    bannerImg: bannerImg,
+    ownerId: 2,
   }
 
   const postNum = getRandom(1, 25)
   const posts = []
   const adjectives = [['Awful', 'Abysmal', 'Terrible'], ['Bad', 'Lacking', 'Not-Fun'], ['Mediocre', 'Average', 'Solid'],
 ['Great', 'Nice', 'Good'], ['Fantastic', 'Exquisite', 'Divine']]
-  const catSuffixes = ['Here!', 'Alert', 'Resides Here', 'Floofs about this place']
+  const barAdjectives = ["beer", "food", "drinking", "liquor", "vodka", "martinis", "service", "rum", "ale"]
+  const barSuffixes = ['here!', 'at this location.', 'joint.', 'spot!', "place"]
   const businessSuffixes = ['Service', 'Spot', 'Place']
   const userNumArr = []
   for (let i = 3; i < 101; i++) {
@@ -95,34 +122,28 @@ const generateCatPost = async (id) => {
       mid = extraInfo.businessCategory
       end = randomArr(businessSuffixes)
     } else {
-      mid = randomArr(catAdjectives)
-      end = randomArr(catSuffixes)
+      mid = randomArr(barAdjectives)
+      end = randomArr(barSuffixes)
     }
     const title = `${adj} ${mid} ${end}`
     const post = {
-      title,
+      overall: rating,
+      food: Math.min(Math.max(rating + getRandom(-1, 1), 1), 5),
+      service: Math.min(Math.max(rating + getRandom(-1, 1), 1), 5),
+      ambience: Math.min(Math.max(rating + getRandom(-1, 1), 1), 5),
+      value: Math.min(Math.max(rating + getRandom(-1, 1), 1), 5),
       userId,
       rating,
-      locationId:id,
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+      barId:id,
+      review: title
     }
     posts.push(post)
-    postNumber++
-
-    const imNum = getRandom(1, 5)
-
-    for (let i = 0; i < imNum; i++) {
-      const url = await getCatImage(breed.id)
-      const title = `${randomArr(catPrefixes)} ${randomArr(catAdjectives)}`
-      images.push({url, title, locationId:id, userId})
-    }
   }
 
-  const averageReview = posts.reduce((acc,el) => acc+el.rating, 0)/posts.length
-  businessInfo.averageRating = averageReview;
-  businessInfo.reviewNumber = posts.length;
   const finalObj = { businessInfo, images, extraInfo, posts }
-//   console.log(finalObj)
+  // console.log(finalObj)
+  console.log(`${finalObj.businessInfo.name} : (${id}/50)`)
+
   return finalObj
 }
 
@@ -135,7 +156,7 @@ const writeIt = async () => {
     users = await makeUsers()
     let data = []
     for(let i = 1; i <=50; i++) {
-        const val = await generateCatPost(i)
+        const val = await generateBar(i)
         data.push(val)
     }
     let businessSeeder = []
@@ -156,4 +177,4 @@ const writeItTest = async () => {
   fs.writeFileSync('./seed-data.json', JSON.stringify({userSeeder: users}))
 }
 
-writeItTest()
+writeIt()
