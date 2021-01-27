4dc04b228e417829a707053d17979982380eccf3
Erick-Bravo <erickbrvo@gmail.com>
2021-01-27 00:36:41 -0800

    stopping point at favorite table query for user and bars

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session
 from flask_login import login_required
-from app.models import User
+from app.models import User, Bar
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,9 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+# @user_routes.route('<int:id>/favorites')
+# def user(id):
+#     favorites = Bar.query.filter(Bar.userId.any(userId=id)).all()
+#     return favorites.to_dict()
