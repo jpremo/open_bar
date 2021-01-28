7198bbef0bab7f7acd2e47f9784cc916bc80e51c
Erick-Bravo <erickbrvo@gmail.com>
2021-01-28 00:40:54 -0800

    save point for some css and userFav cards

--- react-app/src/components/UserProfile/Favorites/index.js
+++ react-app/src/components/UserProfile/Favorites/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from "react-router-dom";
+import { useParams, NavLink } from "react-router-dom";
 // import { useParams } from "react-router-dom";
 import { fetchUserFavorites } from "../../../store/favorites"
 import "./index.css"
@@ -8,16 +8,33 @@ import "./index.css"
 
 
 
+const FavoriteCards = ({ userFav }) => {
+    console.log(userFav)
+    const id = userFav.id
+    const img = userFav.bannerImg
+    const name = userFav.name
+    const seats = userFav.barSeats
+    const street = userFav.street
+    const state = userFav.state
+    const phone = userFav.phoneNumber
 
 
-
-
-
-
-
-
-
-
+    return (
+        <>
+            <div id="card">
+                <NavLink to={`/bars/${id}`}>
+                    <img alt="nope" src={img} />
+                    <div id="bar-container">
+                        <h4>{name}</h4>
+                        <h5>{street}</h5>
+                        <h5>{phone}</h5>
+                        <h5>{`Total Number of Seats ${seats}`}</h5>
+                    </div>
+                </NavLink>
+            </div>
+        </>
+    )
+}
 
 
 
@@ -34,17 +51,15 @@ const Favorites = () => {
     }, [dispatch])
 
     const userFavorites = useSelector(state => state.userFavorites)
-    console.log(userFavorites)
-
-    // if (userId) {
-    //     return null;
-    // }
 
     return (
         <>
             <div id="favorite-container">
                 <h4>Users FAVORITES</h4>
-                {/* {!userState && <h3>state not found</h3>} */}
+                {!userFavorites && <p>Loading...</p>}
+                {userFavorites && userFavorites.map(userFav => {
+                    return <FavoriteCards userFav={userFav} />
+                })}
             </div>
         </>
     )
