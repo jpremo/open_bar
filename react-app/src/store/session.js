28d01af61534a1e56b5dda9e93619c5914a2337b
Jacob <premojacob@gmail.com>
2021-01-29 15:41:45 -0500

    removed debugger

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -14,7 +14,6 @@ export const removeUser = () => ({
 
 export const restoreUser = () => async (dispatch) => {
     let res = await authenticate();
-    debugger
     let data = res
     if(data.errors) data = null
     dispatch(setUser(res));
